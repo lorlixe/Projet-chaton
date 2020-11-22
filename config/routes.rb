306c3b4d50c35a88ba7f218e0b1dcdf8d489645d
marine <marinekouadio@gmail.com>
2020-11-22 22:39:03 +0100

    create controller products

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  "home#index"
-
+  root 'products#index' 
 end
