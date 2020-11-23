08333dab39a45b682e56958f1fd5ab7d1de92acd
Samira Adidaoui <samira.adidaoui@gmail.com>
2020-11-23 18:03:25 +0100

    Mailer created

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config/environments/production.rb :   config.action_mailer.default_url_options = { :host => 'https://chaton-thp2020.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://chaton-thp2020.herokuapp.com/' }
   
 end
