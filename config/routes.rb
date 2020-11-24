54ac306d02306ccd94ef4841ebc9e0f11c212d4e
GRDTHP <redonguillaume@hotmail.fr>
2020-11-24 12:43:19 +0100

    product_routes updated-done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-
-  root 'products#index'
-
-  get 'products/:id', to: 'products#show', as: :product
+    root 'products#index'
+    resources :products
 
 end
