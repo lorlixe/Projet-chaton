13a3de86ecd0c71b5e353bef60608c1b75964401
GRDTHP <redonguillaume@hotmail.fr>
2020-11-24 12:02:23 +0100

    product pages and link with this show page -done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,12 @@
 class ProductsController < ApplicationController
     def index
+      @product = Product.all
+
+    end
+
+    def show
+      @specific_product = Product.find_by_id(params[:id])
+
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  root 'products#index' 
+  root 'products#index'
+
+  get 'products/:id', to: 'products#show', as: :product
+
 end
