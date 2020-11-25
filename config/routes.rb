26f64663094d2a556b87c86dec64d4f9dd9c73bd
Arby Brice <brice.arby91@gmail.com>
2020-11-25 21:42:41 +0100

    Users/show done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'products#index'
   devise_for :users
 	resources :contact, only: [:index]
-  resources :products
+	resources :products
+	resources :users, only: [:index]
 end
