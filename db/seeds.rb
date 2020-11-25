77c05120f6bf7f3e83b87a51a23139106d21b8d0
Arby Brice <brice.arby91@gmail.com>
2020-11-25 03:15:57 +0100

    filled seeds.rb with actual attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,6 +42,32 @@ pictures_array = ["https://photos-chatons.s3.eu-west-3.amazonaws.com/kittens+res
 ]
 
 
+cat_names = ["Minette","Tigrou","Mimi","Chipie","Gribouille","Caramel","Caline","Nala","Félix","Simba","Maya","Minou","Mimine","Princesse","Filou","Fripouille","Kitty","Plume","Isis","Réglisse"]
+
+
+description_array=["Un fidèle matou pour vous tenir compagnie, pour chasser les souris et pour exterminer les cafards !",
+"Un brave minou un poil farouche mais qui adore les câlins ! Vous ne pourrez plus vous en passer !", 
+"Cette petite chatte a un caractère de cochon.. Elle ne se laisse pas mener à la baguette mais sera votre plus fidèle ami", 
+"Ancien chat de gouttière récemment converti à la tranquillité domestique... aime toujours se promener sur les toits au clair de lune.",
+"Un matou paresseux et pantouflard, qui passe le plus clair de son temps à roupiller ; il en faut pour tous les goûts !",
+"Ce gentil minou dont le regard vous fait déjà fondre adore les humains et leur compagnie ; il est joueur, facétieux et s'attache très vite.", 
+"Petite boule de poils qui miaule et qui griffe (un peu mais pas trop) recherche bipède affectueux pour passer ses journées", 
+"Un buveur de lait aux ronronnement apaisants qui (nous a-t-il dit) ne perd presque pas ses poils.. qui veut l'adopter ?",
+"Un ex-chat errant qui a renoncé à la vie sauvage pour embrasser la chaleur d'un foyer et d'un maître.. arriverez-vous à le combler ?",
+"Ce frêle minet est coquin comme pas deux ! A toujours plus d'un tour dans son sac !",
+"Un animal racé et élancé, d'une grande élégance.. ses photos ne lui rendent absolument pas justice !",
+"On n'achète pas un chat dans un sac, d'après un dicton.. au fait, t'as pas 100 sacs ?",
+"A bon chat bon rat.. ne faites pas le rat, achetez-le !",
+"L'amour que vous portera ce félin si vous l'adoptez, c'est pas du pipi de chat..",
+"Chat échaudé craint l'eau froide.. facile à comprendre, même quand on n'a pas inventé l'eau chaude ! ",
+"Y a pas photo, celui-là c'est le plus beau !",
+"Vous vous laisserez bien tenter par l'a-chat d'une image volée à cette adorable boule de poils ?",
+"Chacun cherche son chat.. avez-vous trouvé le vôtre ?",
+"Acheter chat en poche, c'est moche.. acheter chat en photo, c'est beau.",
+"L'essentiel, dans la vie, c'est de toujours retomber sur ses pattes."]
+
+category_array=["gros paresseux", "matou tranquille", "petit mais vivace", "tempérament solitaire", "pour les soirées au coin du feu"]
+
 5.times do |i|
     user = User.create(
         first_name: Faker::Name.first_name,
@@ -53,7 +79,7 @@ end
 
 5.times do |i|
     category = Category.create(
-        name: Faker::Creature::Cat.name
+        name: category_array[i]
     )
 
 end
@@ -61,9 +87,9 @@ end
 
 20.times do |i|
 product = Product.create(
-    description: Faker::Lorem.paragraph(sentence_count: 8),
+    description: description_array[i],
     price: rand(1..99),
-    name: Faker::Lorem.sentence,
+    name: cat_names[i],
     url: pictures_array[i],
     category_id: rand(1..5)
     )
