329426253439ef86d70273ed1d6f4465d5528ee6
marine <marinekouadio@gmail.com>
2020-11-26 02:52:43 +0100

    protection fort current user session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  before_action :is_owner?, only: [:show]
+
   def show
-		@user = User.find(params[:id])
+    @user = current_user
+    
   end
+
+  private 
+
+  def is_owner?
+    if current_user.id.to_i != params[:id].to_i
+      flash[:danger] = "Vous ne pouvez pas acceder à cette page"
+      redirect_to root_path
+    end
+  end
+
 end
